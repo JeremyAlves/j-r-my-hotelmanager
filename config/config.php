be001341789d5daea71f3ab9337a6e761a1939a1
rijuu <47478050+rijuu@users.noreply.github.com>
2020-02-18 10:13:24 +0100

    init repository

---
+++ config/config.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Service\ServiceContainer;
+
+$configuration = [
+    'db' => [
+        'dsn'      => 'mysql:dbname=hbhotelmanager;host=localhost;charset=utf8',
+        'username' => 'root',
+        'password' => '',
+    ],
+    'env' => [
+        'base_path' => 'http://localhost/hbhotelmanager-master',
+        'site_name' => 'HB Hotel Manager'
+    ]
+];
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$container = new ServiceContainer($configuration);
+
+require_once __DIR__ . '/routes.php';
